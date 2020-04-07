4ba7fc7e6fe919302f10845278e8208c6841c957
Dimitrii Lopanov <sexykdi@gmail.com>
2020-04-07 12:18:20 +0300

    upd readme and main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,14 +1,42 @@
 package main
 
 import (
+	"flag"
 	"log"
+	"net"
 	engine "plates_recognition_grpc"
 )
 
+var (
+	// License plates
+	platesConfig  = flag.String("platesConfig", "./data/license_plates_inference.cfg", "Path to LICENSE_PLATES network layer configuration file. Example: yolov3-plates.cfg")
+	platesWeights = flag.String("platesWeights", "./data/license_plates_15000.weights", "Path to weights file. Example: yolov3-plates.weights")
+
+	// OCR
+	ocrConfig  = flag.String("ocrConfig", "./data/ocr_plates_inference.cfg", "Path to OCR network layer configuration file. Example: yolov3-ocr.cfg")
+	ocrWeights = flag.String("ocrWeights", "./data/ocr_plates_7000.weights", "Path to weights file. Example: yolov3-ocr.weights")
+
+	// gRPC port
+	portConfig = flag.String("port", "50051", "Port to listen")
+)
+
 func main() {
-	net, err := engine.NewYOLONetwork("", "", "", "")
+	flag.Parse()
+	if *platesConfig == "" || *platesWeights == "" || *ocrConfig == "" || *ocrWeights == "" {
+		flag.Usage()
+		return
+	}
+
+	netw, err := engine.NewYOLONetwork("", "", "", "")
 	if err != nil {
 		log.Fatalln(err)
 	}
-	_ = net
+	_ = netw
+
+	stdListener, err := net.Listen("tcp", "0.0.0.0:"+*portConfig)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	_ = stdListener
 }
